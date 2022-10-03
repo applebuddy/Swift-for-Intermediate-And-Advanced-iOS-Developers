8de00ae580f88f9dd52e2d0af4edd4d4e49c0eee
applebuddy <eagermin92@gmail.com>
2022-10-04 02:40:46 +0900

    Study lecture 45. Performing Asynchronous Action from Synchoronous Code
    study how to use task function to handle Tasks with await and asynchronous environment.

--- NewsApp/NewsApp/Views/NewsSourceListScreen.swift
+++ NewsApp/NewsApp/Views/NewsSourceListScreen.swift
@@ -11,50 +11,55 @@
 import SwiftUI
 
 struct NewsSourceListScreen: View {
+  
+  @StateObject private var newsSourceListViewModel = NewsSourceListViewModel()
+  
+  var body: some View {
     
-    @StateObject private var newsSourceListViewModel = NewsSourceListViewModel()
-    
-    var body: some View {
-        
-        NavigationView {
-        
-        List(newsSourceListViewModel.newsSources, id: \.id) { newsSource in
-            NavigationLink(destination: NewsListScreen(newsSource: newsSource)) {
-                NewsSourceCell(newsSource: newsSource)
-            }
+    NavigationView {
+      
+      List(newsSourceListViewModel.newsSources, id: \.id) { newsSource in
+        NavigationLink(destination: NewsListScreen(newsSource: newsSource)) {
+          NewsSourceCell(newsSource: newsSource)
         }
-        .listStyle(.plain)
-        .task({ // 뷰가 onAppear 상태일때, async한 동작을 하고자 할때 사용한다.
+      }
+      .listStyle(.plain)
+      .task({ // 뷰가 onAppear 상태일때, async한 동작을 하고자 할때 사용한다.
+        await newsSourceListViewModel.getSources()
+      })
+      .onAppear {
+        //            newsSourceListViewModel.getSources()
+      }
+      .navigationTitle("News Sources")
+      .navigationBarItems(trailing: Button(action: {
+        // MARK: 45. Performing Asynchronous Action from Synchronous Code
+        // task를 사용하면 블럭 내에서 await 예약어를 사용한 비동기 동작을 수행하여 비동기적으로 결과를 기다릴 수 있다.
+        task {
+          // refresh the news
           await newsSourceListViewModel.getSources()
-        })
-        .onAppear {
-//            newsSourceListViewModel.getSources()
         }
-        .navigationTitle("News Sources")
-        .navigationBarItems(trailing: Button(action: {
-            // refresh the news
-        }, label: {
-            Image(systemName: "arrow.clockwise.circle")
-        }))
-        }
+      }, label: {
+        Image(systemName: "arrow.clockwise.circle")
+      }))
     }
+  }
 }
 
 struct NewsSourceListScreen_Previews: PreviewProvider {
-    static var previews: some View {
-        NewsSourceListScreen()
-    }
+  static var previews: some View {
+    NewsSourceListScreen()
+  }
 }
 
 struct NewsSourceCell: View {
-    
-    let newsSource: NewsSourceViewModel 
-    
-    var body: some View {
-        VStack(alignment: .leading, spacing: 10) {
-            Text(newsSource.name)
-                .font(.headline)
-            Text(newsSource.description)
-        }
+  
+  let newsSource: NewsSourceViewModel
+  
+  var body: some View {
+    VStack(alignment: .leading, spacing: 10) {
+      Text(newsSource.name)
+        .font(.headline)
+      Text(newsSource.description)
     }
+  }
 }
