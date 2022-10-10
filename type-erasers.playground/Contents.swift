6c9ef2b52c7cd03741d6b0dcc6cb509f61742b28
applebuddy <eagermin92@gmail.com>
2022-10-11 02:20:49 +0900

    Study lecture 65. Opaque Type returning and printing output usage review in swift

--- type-erasers.playground/Contents.swift
+++ type-erasers.playground/Contents.swift
@@ -11,6 +11,7 @@ protocol ConfigurableCell {
   // associatedtype Model을 선언해서 ConfigurableCell을 채택한 객체가 개별적으로 Model의 타입을 정의해서 그에 맞게 protocol을 준수하는 configure 메서드를 사용할 수 있다.
   associatedtype Model
   func configure(_ model: Model)
+  func identifier() -> String
 }
 
 struct User {
@@ -23,6 +24,10 @@ class StudentCell: ConfigurableCell {
   func configure(_ model: Model) {
     self.model = model
   }
+  
+  func identifier() -> String {
+    self.model?.name ?? ""
+  }
 }
 
 class StaffCell: ConfigurableCell {
@@ -31,11 +36,15 @@ class StaffCell: ConfigurableCell {
   func configure(_ model: Model) {
     self.model = model
   }
+  
+  func identifier() -> String {
+    self.model?.name ?? ""
+  }
 }
 
 // - error : Protocol 'ConfigurableCell' can only be used as a generic constraint because it has Self or associated type requirements
 // let cells: [ConfigurableCell] = [StudentCell(), StaffCell()]
-
+/*
 class AnyConfigurable: ConfigurableCell {
   typealias Model = Any
   private let configureCellClosure: (Any) -> Void
@@ -52,8 +61,8 @@ class AnyConfigurable: ConfigurableCell {
 }
 
 let cells = [AnyConfigurable(StudentCell()), AnyConfigurable(StaffCell())]
-
-// StudentCell, StaffCell이 ConfigurableCell을 채택하고 있지만, 본 타입을 반환타입으로 명시해야만. 반환할 수 없다. 본 타입을 숨기고 반환할 수 없을까?
+*/
+// StudentCell, StaffCell이 ConfigurableCell을 채택하고 있지만, 채택한 프로토콜타입을 메서드의 반환타입으로 사용할 수 없다. 본 타입을 숨기고 반환할 수 없을까?
 /*
 // 컴파일 에러 발생
 func getStudentCell() -> ConfigurableCell {
@@ -65,21 +74,27 @@ func getStaffCell() -> ConfigurableCell {
 }
  */
 
-// 컴파일 에러 발생
+// Opaque type으로 반환타입을 지정하면 상세 타입은 숨기고 반환이 가능하다.
 func getStudentCell() -> some ConfigurableCell {
   let studentCell = StudentCell()
   studentCell.configure(User(name: "Monkey"))
   return studentCell
 }
 
-// 컴파일 에러 발생
 func getStaffCell() -> some ConfigurableCell {
   let staffCell = StaffCell()
   staffCell.configure(User(name: "Dog"))
   return staffCell
 }
+// Opaque type 반환 값을 통해 각자의 상세 identifier 값이 정상적으로 출력된다.
+print("studentCellName : \(getStudentCell().identifier())")
+print("staffCellName : \(getStaffCell().identifier())")
 
+/*
 let studentCell = getStudentCell() as? StudentCell
 let staffCell = getStaffCell() as? StaffCell
 print(studentCell?.model?.name)
 print(staffCell?.model?.name)
+ */
+
+
