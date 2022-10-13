2b65c2b723ef1b08156bda553c3f4c26b3f9caf6
applebuddy <eagermin92@gmail.com>
2022-10-14 04:51:53 +0900

    Study lecture 57. Equatable and Comparable Protocols

--- constraints-generics-example.playground/Contents.swift
+++ constraints-generics-example.playground/Contents.swift
@@ -3,6 +3,22 @@
 import UIKit
 import PlaygroundSupport
 
+// MARK: 57. Equatable and Comparable Protocols
+// Equatable: ==
+// Comparable: < >
+
+func lowest<T: Comparable>(list: [T]) -> T? {
+  let sortedList = list.sorted {
+    // 비교를 위해서는 Comparable protocol을 준수해야 한다. 따라서 제네릭 타입에 Comparable protocol 관련 제약을 추가한다.
+    return $0 < $1
+  }
+  
+  return sortedList.first
+}
+
+print(lowest(list: [4, 5, 6, 1, 200, -100, 999]))
+print(lowest(list: ["a", "c", "d", "b"]))
+
 // MARK: 56. Example 2 - Constraints in Generics
 /*
 struct Movie: Encodable {
@@ -36,3 +52,28 @@ func attack<T: SuperHero>(value: T) {
 let electronMan = Electronman()
 // electronMan은 Fly, Teleport & Strength를 모두 준수하는 SuperHero 프로토콜 타입이므로 인자로 넘겨서 사용이 가능하다.
 attack(value: electronMan)
+
+enum Card: Comparable {
+  case ace
+  case king
+  case queen
+  
+  // 비교 조건 정의
+  static func <(lhs:  Card, rhs: Card) ->  Bool {
+    switch(lhs, rhs) {
+    case (king, ace): return true
+    case (queen, king): return true
+    case (queen, ace): return true
+    default: return false
+    }
+  }
+}
+
+let ace = Card.ace
+let queen = Card.queen
+
+if queen < ace { // queen < ace인경우 true를 반환한다.
+  print("queen < ace")
+}
+
+print(lowest(list: [Card.ace, Card.queen, Card.king]))
