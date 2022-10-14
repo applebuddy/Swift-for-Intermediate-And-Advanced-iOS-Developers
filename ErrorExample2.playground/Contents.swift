ece71fcbcba95c5b1da55ab8b7ab82f00c691b81
applebuddy <eagermin92@gmail.com>
2022-10-15 04:48:06 +0900

    Review using async await, async throws, try await

--- ErrorExample2.playground/Contents.swift
+++ ErrorExample2.playground/Contents.swift
@@ -15,40 +15,35 @@ enum NetworkError: Error {
 }
 
 // Result 타입은 Success, Failure 두개의 제네릭타입과 success, failure 두개 케이스로 되어있는 enum 타입이다. error 혹은 수신한 응답값을 상황에 따라 분기하여 콜백에 전달할 수 있다.
-func getPosts(completion: @escaping ((Result<[Post], NetworkError>)) -> Void) {
+func getPosts() async throws -> [Post] {
   guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {
-    completion(.failure(.badURL))
-    return
+    throw NetworkError.badURL
   }
   
-  URLSession.shared.dataTask(with: url) { data, response, error in
-    if let error = error {
-      completion(.failure(.custom(error)))
-    } else if (response as? HTTPURLResponse)?.statusCode != 200 {
-      completion(.failure(.badRequest))
-    } else {
-      guard let data = data else {
-        completion(.failure(.noData))
-        return
-      }
-      
-      let posts = try? JSONDecoder().decode([Post].self, from: data)
-      if let posts = posts {
-        completion(.success(posts))
-      } else {
-        completion(.failure(.decodingError))
-      }
-    }
+  do {
+    // await은 반환타입 앞에 async가 붙은 메서드에만 사용한다.
+    let data = try await URLSession.shared.data(from: url).0
+    // decode는 단순 throws 메서드이므로 try만 붙여서 값을 파싱하면 된다.
+    let response = try JSONDecoder().decode([Post].self, from: data)
+    return response
+  } catch {
+    throw error
   }
-  .resume()
 }
 
-getPosts { result in
-  // completion handler로 전달받은 result enum 타입을 통해 실패여부 판단 및 수신한 응답값을 확인 가능
-  switch result {
-  case .success(let posts):
-    print(posts)
-  case .failure(let error):
-    print(error)
+func asyncAwaitExample() async -> Task<[Post]?, Never> {
+  Task {
+    do {
+      let posts = try await getPosts()
+      print(posts)
+      return posts
+    } catch {
+      return nil
+    }
   }
+}
+
+Task {
+  let taskResult = await asyncAwaitExample().result
+  print(taskResult)
 }
