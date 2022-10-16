cd686047950807eba72d839f018e27b181cf122e
applebuddy <eagermin92@gmail.com>
2022-10-16 19:31:20 +0900

    Study lecture 18. Enums Example 2 - Teacher and Student
    sometimes, constructing data model using enum type is better choice than using class subclassinig. if it is possible to change common members for multiple objects, or if we don't wanna use type castinig way or handle default statement in switch case to use specific types.

--- EnumInsteadOfSubclassing.playground/Contents.swift
+++ EnumInsteadOfSubclassing.playground/Contents.swift
@@ -1,8 +1,100 @@
+import UIKit
+
+// MARK: 18. Enums Example 2 - Teacher and Student
+// 1) 공통 멤버가 일정치 않고, 언제든 바뀔 수 있다면, enum을 사용할 경우 class subclassing에 비해 보다 이점을 취할 수가 있다.
+// 2) 특정 타입이 생기거나, 공통멤버가 바뀌는 경우에 대한 수정 소요가 class subclassing 방식보다 enum이 적다.
+// 3) 또한 enum을 사용하면 다양한 케이스 객체를 하나의 타입에 담아서 switch문에 사용했을때 default 분기가 생기지 않아 보다 명확한 케이스 분기를 구성할 수 있으며, 타입 내의 다양한 객체를 사용하기 위해 타입 캐스팅을 할 필요가 줄어든다.
+
+struct Student {
+  let name: String
+  let courses: [String]
+  let isFullTime: Bool
+}
+
+struct Teacher {
+  let name: String
+  let courses: [String]
+  let isFullTime: Bool
+}
+
+struct Staff {
+  let name: String
+  let isFullTime: Bool
+}
+
+struct International {
+  let name: String
+  let isFullTime: Bool
+  let courses: [String]
+  let countryOfOrigin: String
+}
+
+enum User {
+  case student(Student)
+  case teacher(Teacher)
+  case staff(Staff)
+  case international(International)
+}
+
+func updateProfile(user: User) {
+  switch user {
+  case .student(let student):
+    print(student)
+  case .teacher(let teacher):
+    print(teacher)
+  case .staff(let staff):
+    print(staff)
+  case .international(let international):
+    print(international)
+  }
+}
+
+updateProfile(user: .student(Student(name: "Monggu", courses: ["Math", "Science"], isFullTime: false)))
+/*
+// class subclassing을 했는데 User를 상속하는 새로 추가된 객체가 일부 공통 멤버를 사용하지 않을때... 생기는 고민...
+class User {
+  var name: String
+//  var courses: [String]
+  var isFullTime: Bool
+  
+  init(name: String, isFullTime: Bool) {
+    self.name = name
+//    self.courses = courses
+    self.isFullTime = isFullTime
+  }
+}
+
+// == ISSUE ==
+// Staff가 course가 필요가 없는데 이를 공통적으로 맞추려면... 부모클래스, User에서 courses 멤버를 제거해야한다.
+// 그리고 course 멤버를 가져야 하는 class들에게 별도로 멤버를 추가해야한다..
+class Staff: User {
+  
+}
+
+class Teacher: User {
+  var courses: [String]
+  
+  init(name: String, courses: [String], isFullTime: Bool) {
+    self.courses = courses
+    super.init(name: name, isFullTime: isFullTime)
+  }
+}
+
+class Student: User {
+  var courses: [String]
+  
+  init(name: String, courses: [String], isFullTime: Bool) {
+    self.courses = courses
+    super.init(name: name, isFullTime: isFullTime)
+  }
+}
+*/
+
 // MARK: 17. Enums Example 1 - Airline Ticket
+
+/*
 // * There's a Issue ... 여지껏 공통 변수가 있는 class Ticket을 잘 사용해왔는데... 새로 생긴 BuddyPass란 놈은 price가 필요없다고 한다... Ticket의 공통멤버를 고칠수는 없는 노릇이고 어떻게 해야하지??
 // -> enum을 활용해보자!
-
-import UIKit
 
 /*
 class Ticket {
@@ -117,3 +209,4 @@ func checkIn(ticket: Ticket) {
     print(international)
   }
 }
+*/
